7c92c47860517ca12315a05a6f62cffdd70df7c8
ariefrachmannn <arvolks@gmail.com>
2020-04-22 02:06:54 +0000

    update the repository

--- committer.py
+++ committer.py
@@ -1,6 +1,6 @@
 import subprocess
 
-cp = subprocess.call("git add /home/covid/", shell=True)
+cp = subprocess.call("git add .", shell=True)
 message = "update the repository"
 cp = subprocess.call("git commit -m '{message}'".format(message=message), shell=True)
 cp = subprocess.call("git push -u origin master -f", shell=True)
