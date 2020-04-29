f83bd3bf2ca3982f9f12da4c25e4e1fdfcbff829
redbit <redbit@instance-product.asia-southeast1-b.c.serene-broker-273413.internal>
2020-04-29 17:43:29 +0000

    update the repository

--- committer.py
+++ committer.py
@@ -1,6 +1,5 @@
 import subprocess
 
-#cp = subprocess.call("cd /home/redbit/crawling/covid/", shell=True)
 cp = subprocess.call("git add .", shell=True)
 message = "update the repository"
 cp = subprocess.call("git commit -m '{message}'".format(message=message), shell=True)
